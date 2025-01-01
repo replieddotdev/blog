ebfd7c76b9664e7df5be23b442c4280c1e98ecd9
Daniel Lucas <hi@danlgz.io>
2025-01-01 14:19:05 -0600

    chore: import font from root

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,10 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;400;600&family=Inter:wght@100;400;500;600&display=swap"
+  }
 ];
 
 export const headers = () => {
